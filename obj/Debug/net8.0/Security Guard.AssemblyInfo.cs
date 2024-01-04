f5aef01217334d4032ed4710537d779ed0814da2
Mostafa Shmaisani <shmaisanimostafa@gmail.com>
2024-01-04 18:47:51 +0200

    Change the navbar to make the ending the user's profile image

--- obj/Debug/net8.0/Security Guard.AssemblyInfo.cs
+++ obj/Debug/net8.0/Security Guard.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Security Guard")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93622319a66e80b03d4d8cb89d9abf841516a6a5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b7c2d5f2c1a1755e6b5f9e623385d8132ecc423")]
 [assembly: System.Reflection.AssemblyProductAttribute("Security Guard")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Security Guard")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
