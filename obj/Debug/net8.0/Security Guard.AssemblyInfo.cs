b7470f26691e54cc3fc2cf6aa0dda14bcefcef8b
Mostafa Shmaisani <shmaisanimostafa@gmail.com>
2024-01-14 09:24:53 +0200

    Fix scans counters

--- obj/Debug/net8.0/Security Guard.AssemblyInfo.cs
+++ obj/Debug/net8.0/Security Guard.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Security Guard")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a01af96a75ad7c73fd93836c4d4fe49f5d0d71e1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7676de0c8f30ae8d4ddc5ccf418c14d4e9b8f4d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Security Guard")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Security Guard")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
