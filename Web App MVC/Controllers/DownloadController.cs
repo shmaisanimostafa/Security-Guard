8de3c9649e8ad0ad582dc26e928f11a39425c899
Mostafa Shmaisany <shmaisanymostafa@localhost.localdomain>
2024-08-02 13:04:14 +0300

    Add Download and Pricing Pages

---
+++ Web App MVC/Controllers/DownloadController.cs
@@ -0,0 +1,36 @@
+using Microsoft.AspNetCore.Mvc;
+using Security_Guard.Models;
+using System.Diagnostics;
+using System.Linq;
+
+namespace Security_Guard.Controllers
+{
+    public class DownloadController : Controller
+    {
+        /*
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        */
+        
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // public IActionResult AboutUs()
+        // {
+        //     return View();
+        // }
+
+        // [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        // public IActionResult Error()
+        // {
+        //     return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        // }
+
+    }
+}
