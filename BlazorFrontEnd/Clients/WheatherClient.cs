299f4deffd21e23546aafb36eee490509300855f
Mostafa Shmaisany <shmaisanimostafa@gmail.com>
2024-08-31 18:31:51 +0300

    Edit Blazor FrontEnd

---
+++ BlazorFrontEnd/Clients/WheatherClient.cs
@@ -0,0 +1,31 @@
+﻿using BlazorFrontEnd.Models;
+
+namespace BlazorFrontEnd.Clients
+{
+    public class WheatherClient
+    {
+        private WeatherForecast[]? forecasts;
+
+
+        public void seedForecasts()
+        {
+            var startDate = DateOnly.FromDateTime(DateTime.Now);
+            var summaries = new[] { "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching" };
+            forecasts = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = startDate.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = summaries[Random.Shared.Next(summaries.Length)]
+            }).ToArray();
+        }
+
+        public WeatherForecast[] GetForecast()
+        {
+            if (forecasts == null)
+            {
+                seedForecasts();
+            }
+            return forecasts;
+        }
+    }
+}
