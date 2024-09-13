270d22e9747934af60c03a6c790dce47ccc191c2
Mostafa Shmaisany <shmaisanimostafa@gmail.com>
2024-09-13 08:58:06 +0300

    Edit download page

--- Shared/obj/Debug/net8.0/Shared.AssemblyInfo.cs
+++ Shared/obj/Debug/net8.0/Shared.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Shared")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b013839074354f326310468c31c9be4bc64934e9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f90b67451d2dd3c396eac575d7ad97239687c543")]
 [assembly: System.Reflection.AssemblyProductAttribute("Shared")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Shared")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
